9bcb1ebbbadac6af9fb3f24c25eb9da2de7539fc
Tfauves <tfauvelle@gmail.com>
2021-10-31 15:42:50 -0400

    initial commit.

---
+++ src/main/java/com/careerdevs/vehiclesRESTpractice/Vehicle.java
@@ -0,0 +1,4 @@
+package com.careerdevs.vehiclesRESTpractice;
+
+public class Vehicle {
+}
---
+++ src/main/java/com/careerdevs/vehiclesRESTpractice/VehiclesResTpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs.vehiclesRESTpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclesResTpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiclesResTpracticeApplication.class, args);
+	}
+
+}
