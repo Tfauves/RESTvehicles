5178ea0c52bb33df65fadd12cc11c2fda6c3df40
Tfauves <tfauvelle@gmail.com>
2021-10-31 15:50:30 -0400

    initial commit.

--- src/main/java/com/careerdevs/vehiclesRESTpractice/Vehicle.java
+++ src/main/java/com/careerdevs/vehiclesRESTpractice/Vehicle.java
@@ -1,4 +1,60 @@
 package com.careerdevs.vehiclesRESTpractice;
 
-public class Vehicle {
+public abstract class Vehicle {
+    private Long id;
+    private String make;
+    private String model;
+    private Integer year;
+    private Long currentOdometer;
+
+    public Vehicle() {}
+
+    public Vehicle(Long id, String make, String model, Integer year, Long currentOdometer) {
+        this.id = id;
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.currentOdometer = currentOdometer;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public Long getCurrentOdometer() {
+        return currentOdometer;
+    }
+
+    public void setCurrentOdometer(Long currentOdometer) {
+        this.currentOdometer = currentOdometer;
+    }
+
 }
