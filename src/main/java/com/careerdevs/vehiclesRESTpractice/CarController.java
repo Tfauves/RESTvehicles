fb8a52f121e7a044b6355035cfb6fd53e840eb4d
Tfauves <tfauvelle@gmail.com>
2021-11-01 14:53:17 -0400

    can create with post mapping.

--- src/main/java/com/careerdevs/vehiclesRESTpractice/CarController.java
+++ src/main/java/com/careerdevs/vehiclesRESTpractice/CarController.java
@@ -1,10 +1,7 @@
 package com.careerdevs.vehiclesRESTpractice;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -51,6 +48,14 @@ public class CarController {
     @GetMapping("/{id}")
     public Car car(@PathVariable Long id) {
         return cars.get(id);
+    }
+
+    @PostMapping
+    public Car newCar(@RequestBody Car newCar) {
+        Long id = idCounter.incrementAndGet();
+        newCar.setId(id);
+        cars.put(id, newCar);
+        return newCar;
     }
 
 
